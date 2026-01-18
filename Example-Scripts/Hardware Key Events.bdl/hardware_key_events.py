79409a4b55789d11e7913b88b932db4dfc63f1b7
vuong4-design <inctrancvuong4@gmail.com>
2026-01-18 13:37:39 +0700

    Add toasts to hardware key example

--- Example-Scripts/Hardware Key Events.bdl/hardware_key_events.py
+++ Example-Scripts/Hardware Key Events.bdl/hardware_key_events.py
@@ -5,6 +5,7 @@ from zxtouch.hardwarekeytypes import (
     HARDWARE_KEY_VOLUME_DOWN,
     HARDWARE_KEY_LOCK,
 )
+from zxtouch.toasttypes import TOAST_WARNING
 import time
 
 
@@ -17,10 +18,20 @@ def press_key(device, key_type, delay=0.2):
 def main():
     device = zxtouch("127.0.0.1")
 
+    device.show_toast(TOAST_WARNING, "Testing hardware key events...", 2)
+    time.sleep(2)
+
     press_key(device, HARDWARE_KEY_VOLUME_UP)
+    device.show_toast(TOAST_WARNING, "Volume up pressed", 2)
+    time.sleep(1)
     press_key(device, HARDWARE_KEY_VOLUME_DOWN)
+    device.show_toast(TOAST_WARNING, "Volume down pressed", 2)
+    time.sleep(1)
     press_key(device, HARDWARE_KEY_HOME)
+    device.show_toast(TOAST_WARNING, "Home pressed", 2)
+    time.sleep(1)
     press_key(device, HARDWARE_KEY_LOCK)
+    device.show_toast(TOAST_WARNING, "Lock pressed", 2)
 
     device.disconnect()
 
